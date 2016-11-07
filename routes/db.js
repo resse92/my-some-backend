971be205477eafe562d424dbeb089dbbd34307cc
AlexZHU <zrl920825@hotmail.com>
2016-11-07 11:59:11 +0800

    删除typings.json,不需要

--- routes/db.js
+++ routes/db.js
@@ -7,7 +7,7 @@ const setting = require("../setting.js");
 //查询数据
 exports.find = async function (collection, query) {
   let db = await MongoClient.connect(setting.db_url);
-  let r = await db.collection(collection).find(query).toArray();
+  let r = await db.collection(collection).find(query).toArray();//转成数组后可以直接传给前端,不转数组各种没用
   return r;
 };
 
